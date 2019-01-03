ffe99fcad7fe5cb1da2b72eaa383cdda5085959a
wangchao <wchao0829@163.com>
2019-01-03 09:55:15 +0800

    first commit

--- app/src/main/java/com/example/wangchao/androidcamera1view/CameraActivity.java
+++ app/src/main/java/com/example/wangchao/androidcamera1view/CameraActivity.java
@@ -104,11 +104,9 @@ public class CameraActivity extends BaseActivity implements ICameraImpl {
     public void handleMsg(Message msg) {
         switch (msg.what){
             case GlobalAction.SAVE_VIDEO_DIALOG_SHOW:
-                Log.d("msg","----SAVE_VIDEO_DIALOG_SHOW-------------");
                 mCameraManager.showProgress(getResources().getString(R.string.save_video));
                 break;
             case GlobalAction.SAVE_VIDEO_DIALOG_DISMISS:
-                Log.d("msg","-------SAVE_VIDEO_DIALOG_DISMISS----------");
                 if(mCameraManager.isShowingProgress()){
                     mCameraManager.dismissProgress();
                 }
